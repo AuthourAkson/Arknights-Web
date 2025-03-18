97d560e9d73491c9812ba11de4d0e4bf3c9a2f4f
AuthourAkson <authourakson@gmail.com>
2025-03-18 18:13:04 +0800

    imitate the arknights official web

---
+++ Login/Login.js
@@ -0,0 +1,16 @@
+        // if判断密码,用户名
+        function give() {
+            var a, b, c;
+
+            a = document.getElementById("user").value;
+            b = document.getElementById("pass").value;
+            if (a == "Authour" && b == 20230901) {
+                location.replace("./Login/load/load.html");
+            }
+            else {
+                alert("s o m e t h i n g  w o r o n g , s a d :（")
+            }
+
+
+        }
+        alert("密码在左下角的白条里有显示哦\n我才不告诉你用户名是Authour密码是20230901呢\n=￣ω￣=\n点击\"确定\"进入第一个页面咯(o・ω・o)")
